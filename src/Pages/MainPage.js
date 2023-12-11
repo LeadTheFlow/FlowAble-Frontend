4e05b3fd885160dee39b4d691e9324eceaa39e42
221B0825 <eunseoyu0825@gmail.com>
2023-12-11 14:19:53 +0900

    feat: api response

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -12,6 +12,22 @@ const MainContainer = styled.div`
 `;
 
 const MainPage = () => {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:5000/api/data");
+      const jsonData = await response.json();
+      setData(jsonData);
+    } catch (error) {
+      console.error("Error", error);
+    }
+  };
+
   const navigate = useNavigate();
 
   function moveToPage(moveTo) {
@@ -20,6 +36,7 @@ const MainPage = () => {
 
   return (
     <MainContainer>
+      <p>{data.message}</p>
       <StyledButton
         value="Calendar"
         onClick={() => moveToPage("Calendar/Selection")}
