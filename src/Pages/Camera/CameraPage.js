842dcd660e02652dbc512be3b0c3379a570a6fd2
221B0825 <eunseoyu0825@gmail.com>
2023-12-12 12:50:15 +0900

    feat: add ResultModal

--- src/Pages/Camera/CameraPage.js
+++ src/Pages/Camera/CameraPage.js
@@ -15,13 +15,20 @@ const CameraContainer = styled.div`
 const CameraPage = () => {
   const location = useLocation();
   const cameraType = location.state?.cameraType || 'Camera';
-  const [resultValue, setResultValue] = useState();
-  const [isColorDetection, setIsColorDetection] = useState(false);
-  const [isProductOCR, setIsProductOCR] = useState(false);
-  const [isRestroomDetection, setIsRestroomDetection] = useState(false);
+  const [responseValue, setResponseValue] = useState({});
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
   const setData = (value) => {
-    setResultValue(value);
+    setResponseValue(value);
+    openModal(); // 데이터를 받아오면 모달을 엽니다.
   };
 
   return (
@@ -30,9 +37,13 @@ const CameraPage = () => {
         <div>
           <p>생리혈 확인 유무 카메라</p>
           <p>화면을 클릭해주세요</p>
-          <FileUploader endpoint="5001/api/color" />
-          {isColorDetection && (
-            <ResultModel setData={resultValue}></ResultModel>
+          <FileUploader setData={setData} endpoint="5001/api/color" />
+          {isModalOpen && (
+            <ResultModel
+              data={responseValue}
+              closeModal={closeModal}
+              cameraType={cameraType}
+            />
           )}
         </div>
       )}
@@ -41,17 +52,15 @@ const CameraPage = () => {
           <p>생리대 제품 인식 카메라</p>
           <p>화면을 클릭해주세요</p>
           <FileUploader setData={setData} endpoint="5002/api/OCR" />
-          <p>result: {resultValue}</p>
+          {isModalOpen && (
+            <ResultModel
+              data={responseValue}
+              closeModal={closeModal}
+              cameraType={cameraType}
+            />
+          )}
         </div>
       )}
-      {/* {cameraType === 'RestroomDetection' && (
-        <div>
-          <p>위생용품 수거함 인식 카메라</p>
-          <p>화면을 클릭해주세요</p>
-          <FileUploader setData={setData} endpoint="" />
-          <p>result: {resultValue}</p>
-        </div>
-      )} */}
     </CameraContainer>
   );
 };
