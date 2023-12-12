8632ff54da8d9251f0225f96c7a58a79900ce416
221B0825 <eunseoyu0825@gmail.com>
2023-12-12 17:04:40 +0900

    feat: LoadingModal css

--- src/components/LoadingModal.js
+++ src/components/LoadingModal.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import styled from 'styled-components';
+import logo from '../logo.svg';
+
+const ModalBackground = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.7); /* 어둡게 하려면 투명도를 조절합니다. */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 9999; /* 다른 모달 등과 겹치지 않게 큰 값으로 설정합니다. */
+`;
+
+const ModalContainer = styled.div`
+  position: relative;
+  background-color: white;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  max-width: 400px;
+  width: 70%;
+  text-align: center;
+`;
 
 const LoadingModal = () => {
   return (
-    <div>
-      <div>로딩중</div>
-    </div>
+    <ModalBackground>
+      <ModalContainer>
+        <img
+          src={logo} /* 로딩 GIF 이미지 경로를 설정합니다. */
+          alt="Loading"
+          width="50"
+          height="50"
+        />
+      </ModalContainer>
+    </ModalBackground>
   );
 };
 
